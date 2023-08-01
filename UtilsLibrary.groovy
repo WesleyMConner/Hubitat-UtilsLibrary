2f20ebd281b0a8f7d8579a49731019cc02e716b3
Wesley M. Conner <wesley.m.conner@gmail.com>
2023-08-01 18:37:08 -0400

    Added trivial banner

--- UtilsLibrary.groovy
+++ UtilsLibrary.groovy
@@ -1,3 +1,19 @@
+// ---------------------------------------------------------------------------------
+// U T I L S   L I B R A R Y
+//
+//  Copyright (C) 2023-Present Wesley M. Conner
+//
+// Licensed under the Apache License, Version 2.0 (aka Apache-2.0, the
+// "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ---------------------------------------------------------------------------------
 import com.hubitat.app.DeviceWrapper as DeviceWrapper
 import com.hubitat.app.DeviceWrapperList as DeviceWrapperList
 import com.hubitat.hub.domain.Event as Event
